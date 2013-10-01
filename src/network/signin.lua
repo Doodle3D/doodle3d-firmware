aab27a97686fc47d65984befc474fb178591e9aa
peteruithoven <peter@peteruithoven.nl>
2013-10-01 12:36:51 +0200

    less logging

--- src/network/signin.lua
+++ src/network/signin.lua
@@ -10,18 +10,15 @@ local M = {}
 --- Signin to connect.doodle3d.com server
 -- 
 function M.signin()
-	log:info("signin")
 	local baseurl = "http://connect.doodle3d.com/signin.php"
 	
 	local localip = wifi.getLocalIP();
-	log:info("localip: "..utils.dump(localip))
 	if localip == nil then
 		log:error("signin failed no local ip found")
 		return
 	end
 	
 	local wifiboxid = wifi.getSubstitutedSsid(settings.get('network.cl.wifiboxid'))
-	log:info("wifiboxid: "..utils.dump(wifiboxid))
 	
 	local cmd = "wget -q -O - "..baseurl.."?wifiboxid="..wifiboxid.."\\&localip="..localip;
 	local output = utils.captureCommandOutput(cmd);

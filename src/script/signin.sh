15f96c115b748276ae75870c0b0d06b22f95e7b8
peteruithoven <peter@peteruithoven.nl>
2014-01-20 15:21:04 +0100

    Start signing in loop after 15 min

--- src/script/signin.sh
+++ src/script/signin.sh
@@ -5,7 +5,7 @@
 # This software is licensed under the terms of the GNU GPL v2 or later.
 # See file LICENSE.txt or visit http://www.gnu.org/licenses/gpl.html for full license details.
 
-sleep 5s
+sleep 15m
 while true; do 
 	/usr/share/lua/wifibox/script/d3dapi signin > /dev/null 2> /dev/null
 	

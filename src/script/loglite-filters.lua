ae65e0841c060229644ed95e1754106db5770ecd
peteruithoven <peter@peteruithoven.nl>
2016-03-16 16:58:32 +0100

    Better print3d and new serial loglite filters

--- src/script/loglite-filters.lua
+++ src/script/loglite-filters.lua
@@ -21,6 +21,25 @@ M.firmware = {
 M.print3d = {
 	['parent'] = 'default',
 	['patterns'] = {
+		['Print 3D server'] = 'byellow',
+		['sendCode%(%)'] = 'green',
+		['readCode%(%)'] = 'blue',
+		['readResponseCode%(%)'] = 'blue'
+	}
+}
+
+-- filter rules for serial communcation of print3d
+M.serial = {
+	['options'] = { mode = 'delete', count = 'none' },
+	['patterns'] = {
+		['Print 3D server'] = 'byellow,_nodelete',
+		['sendCode%(%)'] = 'green,_nodelete',
+		['readCode%(%)'] = 'blue,_nodelete',
+		['readResponseCode%(%)'] = 'blue,_nodelete',
+		['setState%(%)'] = 'bblue,_nodelete',
+		['%[ABSD%]'] = 'gray,_nodelete', -- 0.10.10
+		['%[ABD%]'] = 'gray,_nodelete', -- 0.10.9
+		['%(info%)'] = 'gray,_nodelete' -- 0.10.10
 	}
 }
 
